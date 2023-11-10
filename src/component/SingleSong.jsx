e8e96251dce538c347056e7160bb5db48f888333
Angela Prestano <angiprestano99@gmail.com>
2023-11-10 22:41:19 +0100

    update dimension

--- src/component/SingleSong.jsx
+++ src/component/SingleSong.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const SingleSong = (props) => {
   return (
-    <div className="col text-center px-1 ">
+    <div className="col text-center px-1 mx-2 fs-6">
+      <h5 className="text-white">{props.title_short}</h5>
       <h5 className="text-white">{props.generes}</h5>
       <img className="img-fluid" src={props.poster} alt="moviepicture" />
       <p className="text-white">{props.tracks}</p>
       <span className="text-white">{props.artist}</span>
+      <span className="text-white">{props.duration}</span>
     </div>
   );
 };
